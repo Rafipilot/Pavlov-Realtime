8bdecfa7d6211c5787fa77a7f50841cc44f9a564
Rafayel Latif <rafayel.latif@gmail.com>
2025-05-10 08:56:16 +0100

    add quit logic

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ agent = ao.Agent(Arch=Arch)
 
 
 Run = True
-
 delay = 0.5
 while Run == True:
     time.sleep(delay)
     input_to_agent = [0] * 11
 
     if keyboard.is_pressed('f'):  
-        input_to_agent[0:3] = [1, 1, 1]  # Food
+        input_to_agent[0] = [1]  # Food
+
+    if keyboard.is_pressed("b"):
+        input_to_agent[1:4] = [1,1,1]
+
+    if keyboard.is_pressed("q"):
+        Run = False
 
 
     print("Input to agent: ", input_to_agent)
