caa85e215aaf66fe48ae8e405d17841cf4b896f6
Rafayel Latif <rafayel.latif@gmail.com>
2025-05-27 18:49:29 +0100

    Add support for multiple agents

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ st.set_page_config(layout="centered")
 st.title("Realtime Pavlov App")
 
 if not st.session_state:
-    st.session_state.agent = ao.Agent(Arch=Arch)
+    #st.session_state.agent = ao.Agent(Arch=Arch)
     st.session_state.Run = False
     st.session_state.trial_number = 0
     st.session_state.results = []
+    st.session_state.inputOutputPairs = []
 
 #TUNABLE PARAMETERS
-if st.slider("delay", max_value=10):
-    st.session_state.delay = 1
-if st.slider("trials", max_value=10):
-    st.session_state.trials_at_time = 10
+st.session_state.delay = 1
+st.session_state.trials_at_time = 1
+st.session_state.num_agents = 1
 
-#RANDOM PRETRAINING
-for i in range(4):
-    st.session_state.agent.reset_state(training=True)
+st.session_state.delay = st.slider("delay",min_value=1, max_value=10)
+st.session_state.trials_at_time = st.slider("trials", min_value=1, max_value=10)
+st.session_state.num_agents = st.slider("number of agents", max_value=10, min_value=1)
+
 
 if st.button("Start/ Stop"):
 
@@ -80,12 +81,25 @@ while st.session_state.Run == True:
 
 
     for i in range(st.session_state.trials_at_time):
-        response = st.session_state.agent.next_state(input_to_agent, INSTINCTS=True)
-        st.session_state.agent.reset_state()
-        if response == [1]:
-            responses +=1
+        agent_responses = 0
+        for k in range(st.session_state.num_agents):
+            agent = ao.Agent(Arch, _steps = 10000)
+            #RANDOM PRETRAINING
+            for j in range(4):
+                agent.reset_state(training=True)
+            for j, InOut in enumerate(st.session_state.inputOutputPairs):
+                agent.next_state(InOut[0], InOut[1], INSTINCTS=True)
+                agent.reset_state()
+            response = agent.next_state(input_to_agent, INSTINCTS=True)
+  
+            if response == [1]:
+                responses +=1
 
-    trial_result = {"Trial number ": st.session_state.trial_number , "Input:": input_to_agent, "Agent response:":  (responses/st.session_state.trials_at_time) *100}
+            agent = None 
+        
+
+    trial_result = {"Trial number ": st.session_state.trial_number , "Input:": input_to_agent, "Agent response:":  (responses/(st.session_state.trials_at_time*st.session_state.num_agents)) *100}
+    print("trials at time: ", st.session_state.trials_at_time, "Number of agents: ", st.session_state.num_agents, "Responses: ", responses)
     result.text(trial_result)
     st.session_state.trial_number +=1
     st.session_state.results.append(trial_result)
@@ -106,7 +120,6 @@ st.download_button(
 
 ## TODO downloaad for results
 ## Multiple agents
-## Intensity : B1 , b2 etc
 
 # Trial number Input Result Average(for multiple agents) 
 
