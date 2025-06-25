ba5bd67b8d68f04abbdc26bcfba6d02b21435e1a
Rafayel Latif <rafayel.latif@gmail.com>
2025-06-25 10:58:59 +0200

    Some bug fixes+ debugging

--- Arch.py
+++ Arch.py
@@ -4,7 +4,7 @@
 // aolabs.ai software >ao_core/Arch.py (C) 2023 Animo Omnis Corporation. All Rights Reserved.
 
 Thank you for your curiosity!
-"""
+""" 
 
 
 ## //  Pavlov  -- Reference Design #0
@@ -19,7 +19,7 @@ import ao_core as ar
 import numpy as np
 import random 
 
-number_qa_neurons = 5
+number_qa_neurons = 25
 
 description = "Basic Pavlov"
 arch_i = [1, 3, 1, 3, 1, 1, 1]     # 7 neurons, 1 in each of 6 channels, corresponding to Food, Bell, Tactile, Flash, Chime, Monster, Splash of water
@@ -40,8 +40,10 @@ c0 = 3 # neuron number 3
 def c0_instinct_rule(INPUT, Agent):
     type = "Pleasure"
 
-    if INPUT[0] == 1    and     Agent.buffer[ -2,  Agent.arch.Z__flat[0]]==1 and type== "Pleasure":        # self.Z__flat[0] needs to be adjusted as per the agent, which output the designer wants the agent to repeat while learning postively or negatively
+    if INPUT[0] == 1    and     Agent.buffer[ 0,  Agent.arch.Z__flat[0]]==1 and type== "Pleasure":        # self.Z__flat[0] needs to be adjusted as per the agent, which output the designer wants the agent to repeat while learning postively or negatively
         instinct_response = [1, "c0 instinct triggered"]
+        print("applying pleasure signal")
+        Agent.counter = number_qa_neurons
     elif INPUT[10] == 1 and Agent.story[ Agent.state-1,  Agent.arch.Z__flat[0]] == 1 and type == "Fear":
         instinct_response = [1, "c0 instinct triggered"]                                                                         
     else:
@@ -51,7 +53,7 @@ Arch.C__flat_pleasure = np.append(Arch.C__flat_pleasure, Arch.C__flat[c0])
 Arch.datamatrix[4, Arch.C[1][0]] = c0_instinct_rule
 
 
-# adding instinct control neuron for slight neg
+# adding instinct control neuron for neg
 c1 = 4 #neuron number 3
 
 def c1_instinct_rule(INPUT, Agent):
@@ -62,10 +64,12 @@ def c1_instinct_rule(INPUT, Agent):
         instinct_response = [0, "c1 pass"]
 
     else:
-        if Agent.buffer[ -2,  Agent.arch.Z__flat[0]] == 1: #If reacted without Food or Fear 
+        if Agent.buffer[ 0,  Agent.arch.Z__flat[0]] == 1: #If reacted without Food or Fear 
 
             if Agent.counter ==0:
                 instinct_response = [1, "c1 instinct triggered"]
+                print("applying neg signal")
+                Agent.counter = number_qa_neurons
             else:
                 instinct_response = [0, "c1 pass"]            
 
@@ -85,23 +89,19 @@ def qa0_firing_rule(INPUT, Agent):
     if not hasattr(Agent, 'counter'):
         Agent.__setattr__("counter", 0)
 
-    print("agent counter: ", Agent.counter)
-    print("last response: ", Agent.story[ Agent.state-1,  Agent.arch.Z__flat[0]])
-
-    if Agent.counter < (number_qa_neurons+1) and INPUT[0] == 1    and Agent.buffer[ -2,  Agent.arch.Z__flat[0]] == 1: #If the agent ate food 
+    if Agent.counter < (number_qa_neurons+1) and INPUT[0] == 1    and Agent.buffer[ 0,  Agent.arch.Z__flat[0]] == 1: #If the agent ate food 
 
         Agent.counter += 1
         group_response = np.zeros(number_qa_neurons)
         group_response[0 : Agent.counter] = 1
 
-    elif INPUT[0] == 0    and Agent.buffer[ -2,  Agent.arch.Z__flat[0]] == 1:   
+    elif INPUT[0] == 0    and Agent.buffer[ 0,  Agent.arch.Z__flat[0]] == 1:   
         if Agent.counter == 0:
             ## pain signal needed
             
             print("applying pain signal")
             group_response = np.ones(number_qa_neurons)
             group_response[0 : Agent.counter] = 1
-            Agent.counter = number_qa_neurons
         else:
             print("reducing counter")
             # random_number = random.randint(0, 5)
